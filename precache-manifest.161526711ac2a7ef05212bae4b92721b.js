f43a9ece2e799ebadd25613ccaf3e94f564a3536
playck <playck23@gmail.com>
2020-08-18 00:16:11 +0900

    Updates

---
+++ precache-manifest.161526711ac2a7ef05212bae4b92721b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "154708eb137d67c8cffe9031f39f8eda",
+    "url": "/Movie_app/index.html"
+  },
+  {
+    "revision": "0a4f3be19f3b239bcf21",
+    "url": "/Movie_app/static/css/main.34416067.chunk.css"
+  },
+  {
+    "revision": "cccf043083e35c1f5815",
+    "url": "/Movie_app/static/js/2.e6f564e8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Movie_app/static/js/2.e6f564e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a4f3be19f3b239bcf21",
+    "url": "/Movie_app/static/js/main.013c2df6.chunk.js"
+  },
+  {
+    "revision": "33253572110ec7ea8b07",
+    "url": "/Movie_app/static/js/runtime-main.4f7ee416.js"
+  }
+]);
